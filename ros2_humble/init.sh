db77befabe86cab6512b7592711095980dd7dac8
KZL <lz3450@outlook.com>
2024-02-18 14:45:27 -0600

    ros2 humble

---
+++ ros2_humble/init.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+
+sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt install -y \
+  python3-flake8-docstrings \
+  python3-pip \
+  python3-pytest-cov \
+  ros-dev-tools
+
+sudo apt install -y \
+  python3-flake8-blind-except \
+  python3-flake8-builtins \
+  python3-flake8-class-newline \
+  python3-flake8-comprehensions \
+  python3-flake8-deprecated \
+  python3-flake8-import-order \
+  python3-flake8-quotes \
+  python3-pytest-repeat \
+  python3-pytest-rerunfailures
