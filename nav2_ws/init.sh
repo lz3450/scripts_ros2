47d0ad513e2859ab852946c2a1bef868300a7aca
KZL <lz3450@outlook.com>
2024-02-19 02:15:49 -0600

    nav2

--- nav2_ws/init.sh
+++ nav2_ws/init.sh
@@ -6,49 +6,42 @@ sudo apt update
 sudo apt upgrade -y
 
 sudo apt install \
-    python3-setuptools \
-    libboost-dev\
-    libboost-python-dev \
-    libopencv-dev \
-    python3-numpy \
-    python3-opencv \
-    libboost-python1.74.0 \
-    libgazebo-dev \
-    gazebo \
-    libboost-program-options-dev \
-    libboost-program-options1.74.0 \
-    python3-yaml \
-    libgraphicsmagick++1-dev \
-    graphicsmagick-libmagick-dev-compat \
-    python3-pytest \
-    pkg-config \
     uuid-dev \
+    libboost-all-dev \
+    libeigen3-dev \
+    libode-dev \
+    libflann-dev \
     python3-lxml \
-    libceres-dev \
-    libboost-coroutine-dev \
+    python3-pytest \
+    nlohmann-json3-dev \
     libzmq3-dev \
     libncurses5-dev \
-    libboost-coroutine1.74.0 \
-    libeigen3-dev \
-    libgeographic-dev \
-    python3-catkin-pkg \
-    python3-pyproj \
-    python3-zmq \
-    lcov \
+    libgazebo-dev \
+    gazebo \
+    libceres-dev \
+    python3-setuptools \
+    libxtensor-dev \
+    libomp-dev \
+    libbenchmark-dev \
+    libxsimd-dev \
+    libboost-program-options-dev \
+    libboost-program-options1.74.0 \
+    libopencv-dev \
+    python3-ntplib \
     qtbase5-dev \
     libqt5core5a \
     libqt5gui5 \
     libqt5opengl5 \
     libqt5widgets5 \
-    python3-ntplib \
-    cmake \
-    libboost-all-dev \
-    libode-dev \
-    libflann-dev \
-    libxtensor-dev \
-    libomp-dev \
-    libbenchmark-dev \
-    libxsimd-dev \
-    nlohmann-json3-dev
+    python3-zmq \
+    lcov \
+    libboost-dev \
+    libboost-python-dev \
+    python3-numpy \
+    python3-opencv \
+    libboost-python1.74.0 \
+    python3-yaml \
+    libgraphicsmagick++1-dev \
+    graphicsmagick-libmagick-dev-compat
 
 mkdir -p src
