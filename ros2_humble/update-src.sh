c8cecc784605d2971e386dc1b1fa46956b534fd5
KZL <lz3450@outlook.com>
2024-02-18 21:47:46 -0600

    ros2

--- ros2_humble/update-src.sh
+++ ros2_humble/update-src.sh
@@ -8,7 +8,7 @@ sudo apt upgrade -y
 mkdir -p src
 vcs import --input https://raw.githubusercontent.com/ros2/ros2/humble/ros2.repos src
 
-sudo rosdep init
+sudo rosdep init || :
 rosdep update
 
 rosdep install --from-paths src --ignore-src -y --skip-keys "fastcdr rti-connext-dds-6.0.1 urdfdom_headers"
