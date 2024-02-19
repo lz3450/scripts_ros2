6a5f64d3152afe6a0252bab6369077333df0ea1c
KZL <lz3450@outlook.com>
2024-02-18 23:01:20 -0600

    nav2

--- nav2_ws/init.sh
+++ nav2_ws/init.sh
@@ -5,29 +5,4 @@ set -e
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install \
-    nlohmann-json3-dev \
-    libgraphicsmagick++1-dev \
-    graphicsmagick-libmagick-dev-compat \
-    libxtensor-dev \
-    libomp-dev \
-    libboost-program-options-dev \
-    libboost-program-options1.74.0 \
-    python3-zmq \
-    lcov \
-    libceres-dev \
-    python3-pyproj \
-    python3-opencv \
-    libgeographic-dev \
-    libboost-all-dev \
-    libode-dev \
-    libflann-dev \
-    python3-ntplib \
-    libzmq3-dev \
-    libboost-coroutine-dev \
-    libboost-coroutine1.74.0 \
-    # libgazebo-dev \
-    # gazebo
-
 mkdir -p src
-
