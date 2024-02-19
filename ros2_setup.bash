16759574bf946be49b80f030f9c68fec5aa20030
KZL <lz3450@outlook.com>
2024-02-18 21:06:06 -0600

    setup

--- ros2_setup.bash
+++ ros2_setup.bash
@@ -2,4 +2,13 @@
 
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 source ~/Projects/ros2_humble/install/local_setup.bash
-test -f ~/Projects/AutoTrace/ros2_ws/install/local_setup.bash && source ~/Projects/AutoTrace/ros2_ws/install/local_setup.bash
+
+NAV2_SETUP=~/Projects/AutoTrace/nav2_ws/install/local_setup.bash
+if [ -f $NAV2_SETUP ]; then
+    source $NAV2_SETUP
+fi
+
+WS_SETUP=~/Projects/AutoTrace/nav2_ws/install/local_setup.bash
+if [ -f $WS_SETUP ]; then
+    source $WS_SETUP
+fi
