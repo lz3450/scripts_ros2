0ae774efcefa6fda7ab1f4b1345bf2cba59b7a82
KZL <lz3450@outlook.com>
2024-02-18 23:00:58 -0600

    ros2

--- ros2_setup.zsh
+++ ros2_setup.zsh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-source "$HOME/Projects/ros2_humble/install/local_setup.bash"
+source "$HOME/Projects/ros2_humble/install/local_setup.zsh"
 
-# NAV2_SETUP="$HOME/Projects/nav2_ws/install/local_setup.bash"
+# NAV2_SETUP="$HOME/Projects/nav2_ws/install/local_setup.zsh"
 # if [ -f "$NAV2_SETUP" ]; then
 #     echo "nav2_ws"
 #     source "$NAV2_SETUP"
 # fi
 
-TB4_SETUP="$HOME/Projects/turtlebot4_ws/install/local_setup.bash"
+TB4_SETUP="$HOME/Projects/turtlebot4_ws/install/local_setup.zsh"
 if [ -f "$TB4_SETUP" ]; then
     echo "turtlebot4_ws"
     source "$TB4_SETUP"
 fi
 
-WS_SETUP="$HOME/Projects/ros2_ws/install/local_setup.bash"
+WS_SETUP="$HOME/Projects/ros2_ws/install/local_setup.zsh"
 if [ -f "$WS_SETUP" ]; then
-    echo "ros2_ws"
+    echo "nav2_ws"
     source "$WS_SETUP"
 fi
