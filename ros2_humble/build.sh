28ec4e508913f1c13e84dd029b25718ae38f647a
KZL <lz3450@outlook.com>
2024-02-21 00:00:39 -0600

    ros2

--- ros2_humble/build.sh
+++ ros2_humble/build.sh
@@ -4,6 +4,5 @@ set -e
 
 vcs import --input https://raw.githubusercontent.com/ros2/ros2/humble/ros2.repos src
 
-export MAKEFLAGS="-j 4"
-
-colcon build --symlink-install
+export MAKEFLAGS="-j $(nproc)"
+colcon build --symlink-install --executor sequential --packages-skip-build-finished
