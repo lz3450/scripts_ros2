34f9b0022f2bf68d7b75193ddc59fb2338b38756
KZL <lz3450@outlook.com>
2024-02-21 00:22:46 -0600

    build.sh scripts

--- ros2_humble/build.sh
+++ ros2_humble/build.sh
@@ -5,4 +5,4 @@ set -e
 vcs import --input https://raw.githubusercontent.com/ros2/ros2/humble/ros2.repos src
 
 export MAKEFLAGS="-j $(nproc)"
-colcon build --symlink-install --executor sequential --packages-skip-build-finished
+colcon build --symlink-install --packages-skip-build-finished
--- turtlebot4_ws/desktop-build.sh
+++ turtlebot4_ws/desktop-build.sh
@@ -5,4 +5,4 @@ source ../scripts_ros2/ros2_setup.bash
 vcs import --input deps.repos src
 
 export MAKEFLAGS="-j $(nproc)"
-colcon build --symlink-install --executor sequential --packages-skip-build-finished
+colcon build --symlink-install --packages-skip-build-finished
