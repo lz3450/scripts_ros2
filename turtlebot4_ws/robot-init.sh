47beb220c2e41646e225445575fa962b0a8cdfe8
KZL <lz3450@outlook.com>
2024-02-21 10:00:22 -0600

    tb4

--- turtlebot4_ws/robot-init.sh
+++ turtlebot4_ws/robot-init.sh
@@ -6,32 +6,54 @@ sudo apt update
 sudo apt upgrade -y
 
 sudo apt install -y \
-    libopencv-dev  \
-    python3-numpy  \
-    python3-opencv  \
-    libboost-python1.74.0  \
-    libboost-dev  \
-    libboost-python-dev  \
-    libboost-all-dev  \
-    libignition-common4-dev  \
-    libignition-gazebo6-dev  \
-    libignition-plugin-dev  \
-    libusb-1.0-0-dev  \
-    nlohmann-json3-dev  \
-    python3-psutil  \
-    python3-pytest  \
-    python3-setuptools  \
-    libgpiod-dev  \
-    libspnav-dev  \
-    libx11-dev  \
-    spacenavd  \
-    libignition-math6-dev  \
-    libsdl2-dev  \
-    libbluetooth3  \
-    libcwiid1  \
-    libbluetooth-dev  \
-    libcwiid-dev  \
+    libboost-all-dev \
+    libeigen3-dev \
+    libode-dev \
+    libflann-dev \
+    libgraphicsmagick++1-dev \
+    graphicsmagick-libmagick-dev-compat \
+    libopencv-dev \
+    python3-numpy \
+    python3-opencv \
+    libboost-dev \
+    libboost-python-dev \
+    libboost-python1.74.0 \
+    libxtensor-dev \
+    libomp-dev \
+    libbenchmark-dev \
+    libxsimd-dev \
+    libceres-dev \
     python3-ntplib \
-    libgraphicsmagick++1-dev
+    python3-pytest \
+    libusb-1.0-0-dev \
+    nlohmann-json3-dev \
+    uuid-dev \
+    libgpiod-dev \
+    libspnav-dev \
+    libx11-dev \
+    spacenavd \
+    libsdl2-dev \
+    qtbase5-dev \
+    libqt5core5a \
+    libqt5gui5 \
+    libqt5opengl5 \
+    libqt5widgets5 \
+    libboost-program-options-dev \
+    libboost-program-options1.74.0 \
+    libzmq3-dev \
+    libncurses5-dev \
+    python3-setuptools \
+    libignition-common4-dev \
+    libignition-gazebo6-dev \
+    libignition-plugin-dev \
+    lcov \
+    python3-zmq \
+    libignition-math6-dev \
+    python3-yaml \
+    libbluetooth-dev \
+    libcwiid-dev \
+    libbluetooth3 \
+    libcwiid1 \
+    python3-psutil
 
 mkdir -p src
