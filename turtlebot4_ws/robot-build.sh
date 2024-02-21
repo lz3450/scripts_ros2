7ffb35265b9fca8c6c10faef925d6f1ff24ab7a6
KZL <lz3450@outlook.com>
2024-02-21 01:31:43 -0600

    tb4

--- turtlebot4_ws/robot-build.sh
+++ turtlebot4_ws/robot-build.sh
@@ -4,5 +4,5 @@ source ../scripts_ros2/ros2_setup.bash
 
 vcs import --input deps.repos src
 
-export MAKEFLAGS="-j $(nproc)"
-colcon build --symlink-install --packages-skip-build-finished --packages-skip "nav2_system_tests"
+export MAKEFLAGS="-j 2"
+colcon build --symlink-install --parallel-workers 2 --packages-skip-build-finished --packages-skip "nav2_system_tests"
