b0442ada2ce247676cb4fea0253c4abc7756ac3e
KZL <lz3450@outlook.com>
2024-02-21 02:07:21 -0600

    tb4

--- turtlebot4_ws/turtlebot4_setup.sh
+++ turtlebot4_ws/turtlebot4_setup.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-TB4_SETUP_SRC_DIR="src/turtlebot4_setup"
 
-sudo cp -rv "$TB4_SETUP_SRC_DIR"/scripts/* /usr/local/bin/
-sudo cp -v "$TB4_SETUP_SRC_DIR"/udev/*.rules /etc/udev/rules.d/
-sudo cp -rv "$TB4_SETUP_SRC_DIR"/etc/* /etc/
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y socat
+
+sudo sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
+
+# TB4_SETUP_SRC_DIR="src/turtlebot4_setup"
+# sudo cp -rv "$TB4_SETUP_SRC_DIR"/scripts/* /usr/local/bin/
+# sudo cp -v "$TB4_SETUP_SRC_DIR"/udev/*.rules /etc/udev/rules.d/
+# sudo cp -rv "$TB4_SETUP_SRC_DIR"/etc/* /etc/
 
 sudo systemctl enable webserver.service
+
+ros2 run turtlebot4_setup turtlebot4_setup
