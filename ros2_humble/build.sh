5b04056f6c72b5716f24a7a224d31f04ddf34604
KZL <lz3450@outlook.com>
2024-02-20 23:19:39 -0600

    build.sh scripts

--- ros2_humble/build.sh
+++ ros2_humble/build.sh
@@ -3,4 +3,7 @@
 set -e
 
 vcs import --input https://raw.githubusercontent.com/ros2/ros2/humble/ros2.repos src
+
+export MAKEFLAGS="-j 4"
+
 colcon build --symlink-install
--- turtlebot4_ws/robot-build.sh
+++ turtlebot4_ws/robot-build.sh
@@ -3,4 +3,6 @@
 source ../scripts_ros2/ros2_setup.bash
 
 vcs import --input deps.repos src
+
+export MAKEFLAGS="-j 4"
 colcon build --symlink-install --packages-skip "nav2_system_tests"
