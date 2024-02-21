bd2882f8159372a25e35e90b50e6263e1889f1dd
KZL <lz3450@outlook.com>
2024-02-21 11:10:16 -0600

    tb4

--- turtlebot4_ws/desktop-launch/map.sh
+++ turtlebot4_ws/desktop-launch/map.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-
-
 source /etc/turtlebot4/setup.bash
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 # ros2 run controller_manager ros2_control_node 2>&1 &
-ros2 launch turtlebot4_navigation slam.launch.py 2>&1 | tee turtlebot4_navigation.log
+ros2 launch turtlebot4_navigation slam.launch.py sync:=false 2>&1 | tee turtlebot4_navigation.log
